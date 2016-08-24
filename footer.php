c1123ddbf1388a4d98394f9a39d2e828ef97118b
aravindmjgit <aravindnair93@gmail.com>
2016-08-24 12:23:57 +0530

    Visitor counter

--- footer.php
+++ footer.php
@@ -1,6 +1,27 @@
+<?php
+$f = fopen('counter.txt',"r");
+$counterVal = fread($f, filesize($counter_name));
+fclose($f);
+$counterVal = str_pad($counterVal, 5, "0", STR_PAD_LEFT);
+$chars = preg_split('//', $counterVal);
+?>
+<style>
+    .footer img{
+        height: 20px;
+        width: 20px;
+    }
+</style>
 <!--//footer-->
 <div class="footer">
-
+    <div class="col-lg-2 pull-right" style="color: #fff;">
+        <div class="col-lg-12">Visitors Count</div>
+        <img src="counter/<?= $chars[1] ?>.png">
+        <img src="counter/<?= $chars[2] ?>.png">
+        <img src="counter/<?= $chars[3] ?>.png">
+        <img src="counter/<?= $chars[4] ?>.png">
+        <img src="counter/<?= $chars[5] ?>.png">
+        <div class="col-lg-12 col-md-12" style="font-size: 20px; letter-spacing: 1px;"><?php echo $counterVal; ?></div>
+    </div>
     <div class="container">
         <ul class="social">
             <li><a href="#"><i></i></a></li>
--- index.php
+++ index.php
@@ -1,3 +1,4 @@
+<?php include_once 'counter.php'; ?>
 <!DOCTYPE html>
 <html>
     <head>
